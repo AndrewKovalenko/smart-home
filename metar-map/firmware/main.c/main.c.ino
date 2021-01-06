1b2cc8b597b38eab7c0f0c914141895e1bc88bf9
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-01-05 20:02:48 -0800

    add skeleton for wifi access point

--- metar-map/firmware/main.c/main.c.ino
+++ metar-map/firmware/main.c/main.c.ino
@@ -1,27 +1,197 @@
-//#define FASTLED_ESP8266_D1_PIN_ORDER
-#include "FastLED.h"
+#include <AsyncEventSource.h>
+#include <AsyncJson.h>
+#include <SPIFFSEditor.h>
+#include <WebHandlerImpl.h>
+#include <ESPAsyncWebServer.h>
+#include <WebAuthentication.h>
+#include <AsyncWebSynchronization.h>
+#include <AsyncWebSocket.h>
+#include <WebResponseImpl.h>
+#include <StringArray.h>
 
-#define NUM_LEDS 50
-#define OUTPUT_PIN 5 //D1
+#include <ESPAsyncTCP.h>
+#include <tcp_axtls.h>
+#include <async_config.h>
+#include <AsyncPrinter.h>
+#include <ESPAsyncTCPbuffer.h>
+#include <SyncClient.h>
+#include <DebugPrintMacros.h>
 
-CRGB leds[NUM_LEDS];
+#include <ESP8266WiFiGratuitous.h>
+#include <WiFiServerSecure.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFiType.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFiAP.h>
+#include <WiFiClient.h>
+#include <BearSSLHelpers.h>
+#include <WiFiServer.h>
+#include <ESP8266WiFiScan.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiSTA.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiServerSecureAxTLS.h>
 
-void setup() { 
-  FastLED.addLeds<WS2811, OUTPUT_PIN, RGB>(leds, NUM_LEDS); 
-  FastLED.setBrightness(84);
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+const char *ssid = "metar_map";
+const char *password = "qwerty";
+
+#define DHTPIN 5 // Digital pin connected to the DHT sensor
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0; // will store last time DHT was updated
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000;
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP8266 DHT Server</h2>
+  <p>
+    <span class="dht-labels">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">%</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String &var)
+{
+  //Serial.println(var);
+  if (var == "TEMPERATURE")
+  {
+    return String(t);
+  }
+  else if (var == "HUMIDITY")
+  {
+    return String(h);
+  }
+  return String();
 }
 
-void loop() {
-  for(int x = 0; x < NUM_LEDS; x++){
-             leds[x] = CRGB::Green;
-  }
-  FastLED.show();
-  delay(300);
+void setup()
+{
+  // Serial port for debugging purposes
+  Serial.begin(115200);
 
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
 
-  for(int x = 0; x < NUM_LEDS; x++){
-             leds[x] = CRGB::Black;
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/plain", String(t).c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/plain", String(h).c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
   }
-  FastLED.show();
-  delay(300);
 }
