858aea3f6f04f543033c17b52df0a6da6ca2926a
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-01-08 19:50:42 -0800

    make header file working

--- metar-map/firmware/main.c/wifiAccessPoint.h
+++ metar-map/firmware/main.c/wifiAccessPoint.h
@@ -1,5 +1,14 @@
 
-const char wifiCredentialsPage[] PROGMEM = R"htmlcode(
+const char *ssid = "metar_map";
+const char *password = "metarmappass";
+const int channel = 12;
+const int maxConnections = 1;
+
+const IPAddress local_IP(192, 168, 0, 2);
+const IPAddress gateway(192, 168, 0, 1);
+const IPAddress subnet(255, 255, 255, 254);
+
+const char *wifiCredentialsPage PROGMEM = R"htmlcode(
     <html>
 
 <head>
