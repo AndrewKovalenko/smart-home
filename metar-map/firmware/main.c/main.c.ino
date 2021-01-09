faca54389c3e69925f4e7e9403afd5fbe14582c1
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-01-08 19:47:18 -0800

    add network encryption

--- metar-map/firmware/main.c/main.c.ino
+++ metar-map/firmware/main.c/main.c.ino
@@ -1,132 +1,13 @@
-#include <AsyncEventSource.h>
-#include <AsyncJson.h>
-#include <SPIFFSEditor.h>
-#include <WebHandlerImpl.h>
-#include <ESPAsyncWebServer.h>
-#include <WebAuthentication.h>
-#include <AsyncWebSynchronization.h>
-#include <AsyncWebSocket.h>
-#include <WebResponseImpl.h>
-#include <StringArray.h>
-
-#include <ESPAsyncTCP.h>
-#include <tcp_axtls.h>
-#include <async_config.h>
-#include <AsyncPrinter.h>
-#include <ESPAsyncTCPbuffer.h>
-#include <SyncClient.h>
-#include <DebugPrintMacros.h>
-
-#include <ESP8266WiFiGratuitous.h>
-#include <WiFiServerSecure.h>
-#include <WiFiClientSecure.h>
-#include <WiFiClientSecureBearSSL.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WiFiUdp.h>
-#include <ESP8266WiFiType.h>
-#include <CertStoreBearSSL.h>
-#include <ESP8266WiFiAP.h>
-#include <WiFiClient.h>
-#include <BearSSLHelpers.h>
-#include <WiFiServer.h>
-#include <ESP8266WiFiScan.h>
-#include <WiFiServerSecureBearSSL.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiSTA.h>
-#include <WiFiClientSecureAxTLS.h>
-#include <WiFiServerSecureAxTLS.h>
-
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-
-// Import required libraries
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <Hash.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 
 const char *ssid = "metar_map";
-const char *password = "qwerty";
+const char *password = "metarmappass";
+const int channel = 12;
+const int maxConnections = 1;
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
-// Updates DHT readings every 10 seconds
-const long interval = 10000;
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP8266 DHT Server</h2>
-  <p>
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">%</sup>
-  </p>
-</body>
-<script>
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 10000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("humidity").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/humidity", true);
-  xhttp.send();
-}, 10000 ) ;
-</script>
-</html>)rawliteral";
-
-// Replaces placeholder with DHT values
-String processor(const String &var)
-{
-  return "ok";
-}
+IPAddress local_IP(192,168,0,2);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,254);
 
 void setup()
 {
@@ -135,7 +16,8 @@ void setup()
 
   Serial.print("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid);
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  WiFi.softAP(ssid, password, channel, false, maxConnections);
 
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
@@ -143,38 +25,6 @@ void setup()
 
   // Print ESP8266 Local IP Address
   Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  // Start server
-  server.begin();
-  
-      int sta_connected = 0;
-    if (ESPConf.mode == WIFI_STA)
-    {
-        WiFi.mode((WiFiMode)ESPConf.mode);  //WIFI_AP, WIFI_STA, WIFI_AP_STA, WIFI_OFF
-        local_ip = IPAddress(0,0,0,0);
-        WiFi.config(local_ip, local_ip, local_ip);    //Reset to use DHCP
-        WiFi.hostname(ESPConf.host); //This is the hostrname that should be supplied to the DHCP server
-        WiFi.begin(ESPConf.ssid, ESPConf.passwd);
-        if(WiFi.waitForConnectResult() == WL_CONNECTED)
-        {
-            IP2str(ipbuf, WiFi.localIP());
-            MsgDbg = "STA ADDR = " + String(ipbuf);
-            sta_connected = 1;
-        }
-        else
-            MsgDbg = "WiFi connection to " + String(ESPConf.ssid) + " failed! Starting SoftAP instead";
-        SerialDebug.println(MsgDbg);
-    }
-    if ((ESPConf.mode == WIFI_AP) || (!sta_connected)) //Mode WIFI_AP or failed to connect WIFI_STA
-    {
-        WiFi.mode(WIFI_AP);  //WIFI_OFF, WIFI_STA, WIFI_AP, WIFI_AP_STA
-        WiFi.softAPConfig((IPAddress)ESPConf.addr, (IPAddress)ESPConf.addr, netmask);
-        WiFi.softAP(ESPConf.ssid, ESPConf.passwd, ESPConf.wichannel, true);
 }
 
 void loop()
