27bbf768f8bc58a8fde6332890c1369a793023ea
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-11-15 21:37:03 -0800

    test on the device

--- metar-map/src/metar-map/metar-map.ino
+++ metar-map/src/metar-map/metar-map.ino
@@ -1,28 +1,29 @@
 #include <Arduino.h>
-//  #include <ESP8266WiFi.h>
-//  #include <ESP8266WiFiMulti.h>
+#include <FastLED.h>
+#define NUMBER_OF_LEDS 50
 
-// #include "http-client.h"
-#include "ws2811.h"
+struct LedColor
+{
+    uint8_t red;
+    uint8_t blue;
+    uint8_t green;
+};
 
-//  ESP8266WiFiMulti WiFiMulti;
+CRGBArray<NUMBER_OF_LEDS> leds;
+
+void initializeLedStrip() 
+{ 
+    FastLED.addLeds<WS2811,5>(leds, NUMBER_OF_LEDS); 
+}
+
+void setLed(int ledNumber, LedColor color){ 
+    leds[ledNumber] = CRGB(color.red, color.blue, color.green);
+}
 
 void setup() {
 
   Serial.begin(115200);
   initializeLedStrip();
-  // Serial.println();
-  // Serial.println();
-  // Serial.println();
-
-  // for (uint8_t t = 4; t > 0; t--) {
-  //   Serial.printf("[SETUP] WAIT %d...\n", t);
-  //   Serial.flush();
-  //   delay(1000);
-  // }
-
-  // WiFi.mode(WIFI_STA);
-  // WiFiMulti.addAP("BrainBurner", "Sw6%H0mE!");
 }
 
 void loop() {
@@ -32,17 +33,9 @@ void loop() {
      {
        LedColor rgbColor = {color, color, color};
        setLed(i, rgbColor);
-       delay(10000);
+       FastLED.delay(33);
      }    
+
+     leds.fadeToBlackBy(NUMBER_OF_LEDS);
    }
-  
-  // // wait for WiFi connection
-  // if ((WiFiMulti.run() == WL_CONNECTED)) {
-  //   String payload = makeGetCall("https://www.aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=csv&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=KPAE%20KSEA");
-
-  //   Serial.println(payload);
-  // }
-
-  // Serial.println("Wait 10s before next round...");
-  // delay(10000);
 }
