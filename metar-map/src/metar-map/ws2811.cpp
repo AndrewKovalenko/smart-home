a526ec6244b1c851b6a05b88b6819d2798b09cc1
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-11-19 16:27:55 -0800

    start refactoring led strip controller as a class

--- metar-map/src/metar-map/ws2811.cpp
+++ metar-map/src/metar-map/ws2811.cpp
@@ -19,3 +19,22 @@ void applyLedSettings()
     FastLED.show();
 }
 
+class WS2811LedStrip
+{
+private:
+    CRGBArray<NUMBER_OF_LEDS> leds;
+
+public:
+    WS2811LedStrip(uint8_t numberOfLeds);
+    ~WS2811LedStrip();
+};
+
+WS2811LedStrip::WS2811LedStrip(uint8_t numberOfLeds)
+{
+    FastLED.addLeds<WS2811,5>(this.leds, NUMBER_OF_LEDS); 
+}
+
+WS2811LedStrip::~WS2811LedStrip()
+{
+}
+
