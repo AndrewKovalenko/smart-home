67f63ff38740d2840f87f4857cefa2933abc33f6
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-11-21 18:52:36 -0800

    more refactoring

--- metar-map/src/metar-map/ws2811.cpp
+++ metar-map/src/metar-map/ws2811.cpp
@@ -1,33 +1,7 @@
-#include <FastLED.h>
 #include "ws2811.h"
 #define NUMBER_OF_LEDS 50
 
 CRGBArray<NUMBER_OF_LEDS> leds;
-
-void initializeLedStrip() 
-{ 
-    FastLED.addLeds<WS2811,5>(leds, NUMBER_OF_LEDS); 
-}
-
-void setLed(int ledNumber, LedColor color)
-{ 
-    leds[ledNumber] = CRGB(color.red, color.blue, color.green);
-}
-
-void applyLedSettings()
-{
-    FastLED.show();
-}
-
-class WS2811LedStrip
-{
-private:
-    CRGBArray<NUMBER_OF_LEDS> leds;
-
-public:
-    WS2811LedStrip(uint8_t numberOfLeds);
-    ~WS2811LedStrip();
-};
 
 WS2811LedStrip::WS2811LedStrip(uint8_t numberOfLeds)
 {
@@ -36,5 +10,11 @@ WS2811LedStrip::WS2811LedStrip(uint8_t numberOfLeds)
 
 WS2811LedStrip::~WS2811LedStrip()
 {
+}
+
+void WS2811LedStrip:setLedColor(int ledNumber, LedColor color)
+{ 
+    leds[ledNumber] = CRGB(color.red, color.blue, color.green);
+    FastLED.show();
 }
 
--- metar-map/src/metar-map/ws2811.h
+++ metar-map/src/metar-map/ws2811.h
@@ -1,3 +1,5 @@
+#include <FastLED.h>
+
 struct LedColor
 {
     uint8_t red;
@@ -5,6 +7,18 @@ struct LedColor
     uint8_t green;
 };
 
+class WS2811LedStrip
+{
+private:
+    CRGBArray<NUMBER_OF_LEDS> leds;
+
+public:
+    WS2811LedStrip(uint8_t numberOfLeds);
+    ~WS2811LedStrip();
+
+    void setColor();
+};
+
 void initializeLedStrip();
-void setLed(int ledNumber, LedColor color);
+void setLedColor(int ledNumber, LedColor color);
 void applyLedSettings();
