306cd5865ce2c44687697488c282d1cf86908686
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2022-12-30 17:40:49 -0800

    ugly version caused by FastLED update

--- metar-map/src/metar-map/src/boardModeManager/boardManager.cpp
+++ metar-map/src/metar-map/src/boardModeManager/boardManager.cpp
@@ -1,4 +1,6 @@
 #include <ESP8266WiFi.h>
+#include <FastLED.h>
+
 #include "../utils/stringUtils.h"
 #include "../weather/weatherColorCodes.h"
 #include "../httpClient/http-client.h"
@@ -16,61 +18,61 @@
 
 BoardManager::BoardManager(String baseUrl)
 {
-    weatherReadingUrl = buildWeatherRetrievingUrl(baseUrl, metarStations, NUMBER_OF_STATIONS);
-    httpServer = NULL;
-    ledStrip = WS2811LedStrip();
-    _boardMode = readMode();
+  weatherReadingUrl = buildWeatherRetrievingUrl(baseUrl, metarStations, NUMBER_OF_STATIONS);
+  httpServer = NULL;
+  // ledStrip = WS2811LedStrip();
+  _boardMode = readMode();
 }
 
 BoardMode BoardManager::readMode()
 {
-    WiFiCredentials* credentialsSaved = readWifiCredentials();
-    
-    BoardMode boardMode = areCredentialsBlank(*credentialsSaved) ? WiFiSetup : WeatherClient;
-    delete credentialsSaved;
-    return boardMode;
+  WiFiCredentials *credentialsSaved = readWifiCredentials();
+
+  BoardMode boardMode = areCredentialsBlank(*credentialsSaved) ? WiFiSetup : WeatherClient;
+  delete credentialsSaved;
+  return boardMode;
 }
 
 BoardMode BoardManager::boardMode()
 {
-    return _boardMode;
+  return _boardMode;
 }
 
 void BoardManager::startInWiFiSetupMode()
 {
-    startWiFiAccessPoint();
-    httpServer = startAccessPointConfigWebServer();
+  startWiFiAccessPoint();
+  httpServer = startAccessPointConfigWebServer();
 }
 
 void BoardManager::handleHttpClient()
 {
-    if(httpServer != NULL)
-    {
-      httpServer->handleClient();
-    }
+  if (httpServer != NULL)
+  {
+    httpServer->handleClient();
+  }
 }
 
 void BoardManager::connectToWiFiNetwork()
 {
-    Serial.println("Connecting to network");
-    uint8_t connectionAttempts = 0;
-    WiFiCredentials *savedCredentials = readWifiCredentials();
+  Serial.println("Connecting to network");
+  uint8_t connectionAttempts = 0;
+  WiFiCredentials *savedCredentials = readWifiCredentials();
 
-    WiFi.begin(savedCredentials->ssid, savedCredentials->password);
+  WiFi.begin(savedCredentials->ssid, savedCredentials->password);
 
-    while (WiFi.status() != WL_CONNECTED)
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(WIFI_RECONNECT_DELAY);
+    Serial.print(".");
+    connectionAttempts++;
+
+    if (connectionAttempts > MAX_CONNECTION_ATTEMPTS)
     {
-        delay(WIFI_RECONNECT_DELAY);
-        Serial.print(".");
-        connectionAttempts++;
-
-        if (connectionAttempts > MAX_CONNECTION_ATTEMPTS)
-        {
-            resetCredentialsStorage();
-            delete savedCredentials;
-            ESP.restart();
-        }
+      resetCredentialsStorage();
+      delete savedCredentials;
+      ESP.restart();
     }
+  }
 }
 
 void BoardManager::displayWeatherOnTheMap()
@@ -79,33 +81,78 @@ void BoardManager::displayWeatherOnTheMap()
   const uint8_t numberOfStations = (uint8_t)(sizeof(metarStations) / sizeof(metarStations[0]));
   parseResponse(result, metarStations, numberOfStations);
 
-  for (uint8_t i = 0; i < numberOfStations; i++)
+  CRGBArray<50> leds;
+  FastLED.addLeds<WS2811, 5>(leds, 50);
+  FastLED.setBrightness(60);
+
+  for (uint8_t i = 0; i < 50; i++)
   {
-    LedColor colorForCurrentStation;
-
-    switch (metarStations[i].weather[0])
+    bool ledWeatherSet = false;
+    for (uint8_t j = 0; j < numberOfStations; j++)
     {
-    case 'V':
-      colorForCurrentStation = VFR_COLOR;
-      break;
+      if (i == metarStations[j].ledNumber)
+      {
+        // LedColor colorForCurrentStation;
 
-    case 'M':
-      colorForCurrentStation = MVFR_COLOR;
-      break;
+        // switch (metarStations[j].weather[0])
+        // {
+        // case 'V':
+        //   colorForCurrentStation = VFR_COLOR;
+        //   break;
 
-    case 'I':
-      colorForCurrentStation = IFR_COLOR;
-      break;
+        // case 'M':
+        //   colorForCurrentStation = MVFR_COLOR;
+        //   break;
 
-    case 'L':
-      colorForCurrentStation = LIFR_COLOR;
-      break;
+        // case 'I':
+        //   colorForCurrentStation = IFR_COLOR;
+        //   break;
 
-    default:
-      colorForCurrentStation = NO_DATA_COLOR;
+        // case 'L':
+        //   colorForCurrentStation = LIFR_COLOR;
+        //   break;
+
+        // default:
+        //   colorForCurrentStation = NO_DATA_COLOR;
+        // }
+
+        CRGB colorForCurrentStation;
+        switch (metarStations[j].weather[0])
+        {
+        case 'V':
+          colorForCurrentStation = CRGB(34, 197, 1);
+          break;
+
+        case 'M':
+          colorForCurrentStation = CRGB(31, 112, 219);
+          break;
+
+        case 'I':
+          colorForCurrentStation = CRGB(253, 0, 0);
+          break;
+
+        case 'L':
+          colorForCurrentStation = CRGB(251, 63, 255);
+          break;
+
+        default:
+          colorForCurrentStation = CRGB(255, 255, 0);
+        }
+
+        // ledStrip.setLedColor(i, colorForCurrentStation);
+        leds[i] = colorForCurrentStation;
+        Serial.println("Setting " + metarStations[j].stationName + " LED # " + metarStations[j].ledNumber + " to " + metarStations[j].weather);
+        ledWeatherSet = true;
+        break;
+      }
     }
 
-    ledStrip.setLedColor(metarStations[i].ledNumber, colorForCurrentStation);
+    if (!ledWeatherSet)
+    {
+      // ledStrip.setLedColor(i, OFF_COLOR);
+      leds[i] = CRGB(0, 0, 0);
+      Serial.println("Setting LED # " + String(i) + " to OFF");
+    }
     ledStrip.apply();
   }
 }
