73b8ffdcc4217e3ff5309bb92aeb4c0ac522e888
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-02-23 07:03:04 -0800

    configure weather data structures so they properly deserialize

--- metar-map/projects/weather-api-proxy/crates/proxy_application/src/dtos/weather_server_record.rs
+++ metar-map/projects/weather-api-proxy/crates/proxy_application/src/dtos/weather_server_record.rs
@@ -1,22 +1,144 @@
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(untagged)]
 pub(crate) enum WeatherServerVisibility {
     Unrestricted(String),
-    Defined(u8),
-    Undefined(i8),
+    Specific(f32),
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub(crate) struct CloudLayer {
+    #[serde(rename = "cover")]
     pub coverage_type: String,
-    pub bases_altitude: String,
+
+    #[serde(rename = "base")]
+    pub bases_altitude: u16,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub(crate) struct WeatherServerRecord {
+    #[serde(rename = "icaoId")]
     pub station_id: String,
-    pub visibility: WeatherServerVisibility,
+
+    #[serde(rename = "visib")]
+    pub visibility: Option<WeatherServerVisibility>,
+
+    #[serde(rename = "clouds")]
     pub sky_coverage: Vec<CloudLayer>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json;
+
+    const COMPLETE_DATA_SAMPLE: &str = r#"
+    {
+		"metar_id": 501717771,
+		"icaoId": "KEAT",
+		"receiptTime": "2024-02-22 14:16:27",
+		"obsTime": 1708611180,
+		"reportTime": "2024-02-22 14:13:00",
+		"temp": 1.7,
+		"dewp": 0.6,
+		"wdir": 330,
+		"wspd": 5,
+		"wgst": null,
+		"visib": "10+",
+		"altim": 1024.8,
+		"slp": null,
+		"qcField": 6,
+		"wxString": null,
+		"presTend": null,
+		"maxT": null,
+		"minT": null,
+		"maxT24": null,
+		"minT24": null,
+		"precip": null,
+		"pcp3hr": null,
+		"pcp6hr": null,
+		"pcp24hr": null,
+		"snow": null,
+		"vertVis": null,
+		"metarType": "SPECI",
+		"rawOb": "KEAT 221413Z AUTO 33005KT 10SM BKN007 02\/01 A3026 RMK AO2 T00170006",
+		"mostRecent": 1,
+		"lat": 47.3975,
+		"lon": -120.201,
+		"elev": 375,
+		"prior": 3,
+		"name": "Wenatchee\/Pangborn Mem, WA, US",
+		"clouds": [
+			{
+				"cover": "BKN",
+				"base": 700
+			}
+		]
+	}
+    "#;
+
+    #[test]
+    fn successful_parse_weather_record() {
+        let weather_record_parsing_result: Result<WeatherServerRecord, _> =
+            serde_json::from_str(COMPLETE_DATA_SAMPLE);
+
+        assert!(matches!(weather_record_parsing_result, Result::Ok(_)));
+    }
+
+    const VISIBILITY_MISSING_WEATHER_SAMPLE: &str = r#"
+    {
+		"metar_id": 501717771,
+		"icaoId": "KEAT",
+		"receiptTime": "2024-02-22 14:16:27",
+		"obsTime": 1708611180,
+		"reportTime": "2024-02-22 14:13:00",
+		"temp": 1.7,
+		"dewp": 0.6,
+		"wdir": 330,
+		"wspd": 5,
+		"wgst": null,
+		"visib": null,
+		"altim": 1024.8,
+		"slp": null,
+		"qcField": 6,
+		"wxString": null,
+		"presTend": null,
+		"maxT": null,
+		"minT": null,
+		"maxT24": null,
+		"minT24": null,
+		"precip": null,
+		"pcp3hr": null,
+		"pcp6hr": null,
+		"pcp24hr": null,
+		"snow": null,
+		"vertVis": null,
+		"metarType": "SPECI",
+		"rawOb": "KEAT 221413Z AUTO 33005KT 10SM BKN007 02\/01 A3026 RMK AO2 T00170006",
+		"mostRecent": 1,
+		"lat": 47.3975,
+		"lon": -120.201,
+		"elev": 375,
+		"prior": 3,
+		"name": "Wenatchee\/Pangborn Mem, WA, US",
+		"clouds": [
+			{
+				"cover": "BKN",
+				"base": 700
+			}
+		]
+	}
+    "#;
+
+    #[test]
+    fn successful_parse_weather_record_with_missing_visibility() {
+        let weather_record_parsing_result: Result<WeatherServerRecord, _> =
+            serde_json::from_str(VISIBILITY_MISSING_WEATHER_SAMPLE);
+
+        assert!(matches!(weather_record_parsing_result, Result::Ok(_)));
+
+        let parsed_weather_data = weather_record_parsing_result.unwrap();
+        assert!(matches!(parsed_weather_data.visibility, Option::None));
+    }
 }
