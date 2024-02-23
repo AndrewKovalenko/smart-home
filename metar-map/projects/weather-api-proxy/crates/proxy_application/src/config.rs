e582210052a48e3f4839d7168c3ac6ff75bca871
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-02-23 08:28:43 -0800

    request deserialisation fails

--- metar-map/projects/weather-api-proxy/crates/proxy_application/src/config.rs
+++ metar-map/projects/weather-api-proxy/crates/proxy_application/src/config.rs
@@ -1,2 +1,2 @@
 pub const WEATHER_SERVER_URL_TEMPLATE: &str =
-    "https://aviationweather.gov/api/data/metar?ids={stations}&format=json&hours=0";
+    "https://aviationweather.gov/api/data/metar?ids={}&format=json&hours=0";
