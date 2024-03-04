4bc4c9bf7719a33e6da6e3aee140842bb02826ae
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2024-03-03 19:38:46 -0800

    make axum server to read port from ENV

--- metar-map/projects/weather-api-proxy/crates/launchers/metar_data_processor/src/main.rs
+++ metar-map/projects/weather-api-proxy/crates/launchers/metar_data_processor/src/main.rs
@@ -1,24 +1,60 @@
-use std::collections::HashMap;
-use std::env;
-use std::net::Ipv4Addr;
-use warp::{http::Response, Filter};
+use std::{collections::HashMap, env};
+use tokio;
+
+use axum::{
+    extract::Query,
+    {http::StatusCode, routing::get, Json, Router},
+};
+
+use proxy_application::{
+    dtos::weather_category::WeatherCategory,
+    services::weather_service::get_weather_category_for_stations,
+};
+
+const SERVER_ADDRESS: &str = "0.0.0.0";
+const PORT_KEY: &str = "FUNCTIONS_CUSTOMHANDLER_PORT";
+const DEFAULT_PORT: u16 = 3000;
+const STATION_ID_SEPARATOR: &str = ",";
+const STATIONS_PARAMETER_NAME: &str = "stations";
 
 #[tokio::main]
 async fn main() {
-    let example1 = warp::get()
-        .and(warp::path("api"))
-        .and(warp::path("get_flight_category"))
-        .and(warp::query::<HashMap<String, String>>())
-        .map(|p: HashMap<String, String>| match p.get("name") {
-            Some(name) => Response::builder().body(format!("Hello, {}. This HTTP triggered function executed successfully.", name)),
-            None => Response::builder().body(String::from("This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.")),
-        });
-
-    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
-    let port: u16 = match env::var(port_key) {
-        Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
-        Err(_) => 3000,
+    let server_port: u16 = match env::var(PORT_KEY) {
+        Ok(port_value) => port_value.parse().expect("Port value is invalid"),
+        Err(_) => DEFAULT_PORT,
     };
 
-    warp::serve(example1).run((Ipv4Addr::LOCALHOST, port)).await
+    let web_app = Router::new().route(
+        "/getWeatherCategoriesForStations",
+        get(get_weather_categories_for_stations),
+    );
+
+    let server_url = format!("{}:{}", SERVER_ADDRESS, server_port);
+    let io_listener = tokio::net::TcpListener::bind(server_url).await.unwrap();
+    axum::serve(io_listener, web_app).await.unwrap();
+}
+
+async fn get_weather_categories_for_stations(
+    query_parameters: Query<HashMap<String, String>>,
+) -> (StatusCode, Json<Vec<WeatherCategory>>) {
+    let station_ids_query_parameter = query_parameters.get(STATIONS_PARAMETER_NAME);
+
+    if station_ids_query_parameter == Option::None
+        || !station_ids_query_parameter
+            .unwrap()
+            .contains(STATION_ID_SEPARATOR)
+    {
+        return (StatusCode::UNPROCESSABLE_ENTITY, Json(Vec::new()));
+    }
+
+    let station_ids = station_ids_query_parameter
+        .unwrap()
+        .split(STATION_ID_SEPARATOR)
+        .collect::<Vec<&str>>();
+
+    let weather_categories = get_weather_category_for_stations(station_ids.as_slice())
+        .await
+        .unwrap();
+
+    (StatusCode::OK, Json(weather_categories))
 }
